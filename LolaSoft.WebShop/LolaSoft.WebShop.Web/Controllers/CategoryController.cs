081c342665947cf15240c49f7e750c8fc113161b
jelena vojinovic <jelenab71@gmail.com>
2020-01-19 20:17:32 +0100

    Category GetAll() part 2

---
+++ LolaSoft.WebShop/LolaSoft.WebShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using LolaSoft.WebShop.Services.Services;
+using LolaSoft.WebShop.Web.ViewModel.Category;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace LolaSoft.WebShop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService ?? throw new ArgumentNullException(nameof(categoryService));
+        }
+        public IActionResult Index()
+        {
+            var allcategories = categoryService.GetAllWithParentCategory();
+            var categories = allcategories.Select(c =>
+            new CategoryViewModel()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                ParentCategoryName = c.ParentCategory?.Name,
+                ParentCategoryId = c.ParentCategoryId
+            }).ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ LolaSoft.WebShop/LolaSoft.WebShop.Web/ViewModel/Category/CategoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LolaSoft.WebShop.Web.ViewModel.Category
+{
+    public class CategoryViewModel
+    {
+        [Display(Name = "Category ID")]
+        public int Id { get; set; }
+
+        public int? ParentCategoryId { get; set; }
+
+        [Display(Name = " Parent Name")]
+        public string ParentCategoryName { get; set; }
+
+        public string Name { get; set; }
+    }
+}
